6df084f8be000a306b71847bbcf9dcadcad17147
dinhngocnam <namdnfx17199@funix.edu.vn>
2022-08-31 21:43:42 +0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,6 +19,8 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from '../shared/baseUrl'
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -120,7 +122,7 @@ function RenderDish({ dish }) {
           <CardImg
             width="100%"
             object
-            src={dish.image}
+            src={baseUrl + dish.image}
             alt={dish.name}
           ></CardImg>
           <CardBody>
